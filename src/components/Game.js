86b52017d687b2b57380c1ef41ac38ed6aafa5d1
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-28 17:22:39 +0100

    fortfarande inte löst problemet, men närmar mig svaret :)

--- src/components/Game.js
+++ src/components/Game.js
@@ -95,19 +95,32 @@ class Game extends Component {
   checkAnswers = (obj, x) => {
     // let a = Object.keys(obj);
     // console.log(a);
-    console.log(obj);
 
-    for (let [key, value] of Object.entries(obj)) {
-      // console.log(`${key}: ${value}`)
-      if (key === 'choice' + obj.answer) {
-        console.log('thats true');
-      } else {
-        console.log('thats not true');
-      }
-    }
+    let b = 'choice' + obj.answer in obj;
+    console.log(b);
 
-    // for (let i = 1; i <= 4; i++) {
-      
+    // console.log(obj.choice1);
+
+    // console.log('choice' + obj.answer);
+
+    // if ('choice' + obj.answer === obj.choice1) console.log('true answer');
+
+    // obj.find(x => console.log(x))
+
+    // for (let [key, value] of Object.entries(obj)) {
+      // console.log(`${key}: ${value}`);
+      // let b = ''
+      // console.log('choice' + obj.answer);
+      // if (key === 'choice' + obj.answer) console.log('thats true');
+    // }
+
+    // console.log(x);
+    // console.log('rätta svaret!' ,'choice' + obj.answer);
+
+    // if ('choice' + obj.answer === x) {
+    //   console.log('rätt svar');
+    // } else {
+    //   console.log('fel svar');
     // }
   }
 
@@ -137,17 +150,17 @@ class Game extends Component {
               </div>
 
               <div className="answers-div">
-                <input type="radio" id={x.choice2} className="answers" name="question" value={x.choice2} />
+                <input type="radio" id={x.choice2} className="answers" name="question" value={x.choice2} onClick={() => this.checkAnswers(x, x.choice2)} />
                 <label htmlFor={x.choice2}>{x.choice2}</label>
               </div>
 
               <div className="answers-div">
-                <input type="radio" id={x.choice3} className="answers" name="question" value={x.choice3} />
+                <input type="radio" id={x.choice3} className="answers" name="question" value={x.choice3} onClick={() => this.checkAnswers(x, x.choice3)} />
                 <label htmlFor={x.choice3}>{x.choice3}</label>
               </div>
 
               <div className="answers-div">
-                <input type="radio" id={x.choice4} className="answers" name="question" value={x.choice4} />
+                <input type="radio" id={x.choice4} className="answers" name="question" value={x.choice4} onClick={() => this.checkAnswers(x, x.choice4)} />
                 <label htmlFor={x.choice4}>{x.choice4}</label>
               </div>
             </ul>
